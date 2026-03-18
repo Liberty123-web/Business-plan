455c89efdc9800519ef02b4801446af67c3bd8b3
Liberty <chijinduliberty420@gmail.com>
2026-03-18 15:42:59 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Hemo from './Components/Home/Hemo.jsx'
 import About from './Components/about.jsx'
 import Services from './Components/Services.jsx'
 import Pricing from './Components/Pricing.jsx'
-import Contact from './Components/Contact.jsx'
+import Contact from './Components/contact.jsx'
 
 import AboutHome from './Components/Home/AboutHome.jsx'
 import ServiceHome from './Components/Home/ServiceHome.jsx'
 import PriceHome from './Components/Home/PriceHome.jsx'
 import FootHome from './Components/Home/FootHome.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom'
 
 function App() {
 
